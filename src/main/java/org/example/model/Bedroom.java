80f253e6ec6576169754c2c52dc11cb67e0d8305
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-03-26 18:01:45 +0300

    .gitignore updated

--- src/main/java/org/example/model/Bedroom.java
+++ src/main/java/org/example/model/Bedroom.java
@@ -7,8 +7,8 @@ public class Bedroom extends Room {
     private Wardrobe wardrobe;
     private Carpet carpet;
 
-    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling,
-                   Bed bed, Lamp lamp, Wardrobe wardrobe, Carpet carpet) {
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4,
+                   Ceiling ceiling, Bed bed, Lamp lamp, Wardrobe wardrobe, Carpet carpet) {
         super(name, wall1, wall2, wall3, wall4, ceiling);
         this.bed = bed;
         this.lamp = lamp;
