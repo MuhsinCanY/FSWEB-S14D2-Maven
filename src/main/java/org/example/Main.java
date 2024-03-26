ad953730a7f6fb978bff9c2ef514fd5a4301c6e7
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-03-26 13:21:12 +0300

    S14D2 Challenge completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Eğer Room isminde bir sınıf tanımlasaydık
+        // bu Room sınıfı BedRoom sınıfının Parent'ı olurdu
+        // diğer bir değişle BedRoom sınıfı Room sınıfın Child'ı olurdu.
+        //Room sınıfında name,walls ve ceiling instance variable değeri olmalı.
     }
 }
